e098e6babb707b3484bd497caef7df785ef97b89
thaibao02 <thaibaovu0212@gmail.com>
2025-05-22 02:01:22 +0700

    update header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,27 @@
 
 const Header = () => {
     return(
-        <>
-        <h1> header</h1>
-        </>
+        
+        <div className="header">
+            {/* logo */}
+            <div className="logo">
+                <img src="https://saven-serve.s3.amazonaws.com/logo.png" alt="logo" />
+            </div>
+
+            {/* navigation */}
+            <div className="navigation">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/services">Services</a></li>
+                </ul>
+            </div>
+            {/* icon profile */}
+            <div className="profile">
+                <img src="https://saven-serve.s3.amazonaws.com/profile.png" alt="profile" />
+            </div>
+        </div>
     );
 }
 
