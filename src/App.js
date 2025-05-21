eb253ad1456da28521cfbbf36e4f8ac2b843ea85
thaibao02 <thaibaovu0212@gmail.com>
2025-05-21 14:31:05 +0700

    create component and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './App.css';
-
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
 function App() {
   return (
     <>
-    
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
