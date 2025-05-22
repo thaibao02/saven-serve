00bac689559a9fcbf522e89b7975133855c1de8a
thaibao02 <thaibaovu0212@gmail.com>
2025-05-22 21:44:55 +0700

    add image , fix header follow figma

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,3 +1,6 @@
+import { FiPhone } from 'react-icons/fi';
+import logo from '../../Assets/logo1.jpg';
+import SaveNServer from '../../Assets/SaveNServer.png';
 
 const Header = () => {
     return(
@@ -5,17 +8,24 @@ const Header = () => {
         <div className="header">
             {/* logo */}
             <div className="logo">
-                <img src="https://saven-serve.s3.amazonaws.com/logo.png" alt="logo" />
+                <img src={logo} alt="logo" />
+                <img src={SaveNServer} alt="SaveNServer" style={{ height: '20px', marginLeft: '15px' }} />
             </div>
 
             {/* navigation */}
             <div className="navigation">
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                    <li><a href="/services">Services</a></li>
+                    <li><a href="/">Trang chủ</a></li>
+                    <li><a href="/shop">Mua sắm</a></li>
+                    <li><a href="/info">Thông Tin</a></li>
+                    <li><a href="/news">Tin Tức</a></li>
+                    <li><a href="/contact">Liên Hệ</a></li>
                 </ul>
+            </div>
+            {/* phone icon and number */}
+            <div className="phone">
+                <FiPhone style={{ color: 'white', fontSize: '20px', marginRight: '8px' }} />
+                <span style={{ color: 'white' }}>+84 915 824 712</span>
             </div>
             {/* icon profile */}
             <div className="profile">
